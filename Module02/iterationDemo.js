ff9f658329afcf8a1b310c878f77e3f628512853
sailajanaralasetty <98257488+sailajanaralasetty@users.noreply.github.com>
2022-01-31 12:12:40 +0530

    Add files via upload

---
+++ Module02/iterationDemo.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class IterationDemo extends LightningElement {
+    contacts=[{"Id":"C1","Name":"John Abraham","Email":"ja@gmail.com"},
+              {"Id":"C2","Name":"Brad Pitt","Email":"bpitt@gmail.com"},
+              {"Id":"C3","Name":"Angelina Jolie","Email":"angelj@gmail.com"},
+              {"Id":"C4","Name":"Peter Parker","Email":"spiderman@marvelcomics.com"}  ]
+}
